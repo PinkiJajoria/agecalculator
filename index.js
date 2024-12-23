141cda7b42b61ef87ab9d38fe34658a421077e79
PinkiJajoria <99382118+PinkiJajoria@users.noreply.github.com>
2024-12-23 21:38:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const button = document.querySelector('#btn');
 const result = document.querySelector('#spanResult');
 
 function ageCalculate() {
+ 
+
+  if (!date.value) {
+    alert('please enter date of birth');
+    return;
+  }
+  
+
   const currentDate = new Date();
   const dateOfBirth = new Date(date.value);
-  
   // Calculate the difference in time (milliseconds)
-  const ageInMilliseconds = currentDate - dateOfBirth;
+  const years = currentDate.getFullYear() - dateOfBirth.getFullYear();
 
-  // Convert milliseconds to years (1 year = 365.25 days, including leap years)
-  const millisecondsInOneYear = 365.25 * 24 * 60 * 60 * 1000; // milliseconds in one year
-  const years = ageInMilliseconds / millisecondsInOneYear;
+  const months = currentDate.getMonth() - dateOfBirth.getMonth();
 
-  // Output the result, rounded to two decimal places for better readability
-  result.innerHTML = `Your age is ${years.toFixed(2)} years`;
+  const days = currentDate.getDate() - dateOfBirth.getDate();
+
+  if (months < 0) {
+    months += 12;
+    years--;
+  }
+
+
+  if (days < 0) {
+    months--;
+  
+
+  const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() -1, 0)
+days += previousMonth.getDate();
+}
+
+
+  
+  result.innerHTML = `Your age is ${years} years, ${months} months and ${days} days`;
 }
 
 button.addEventListener('click', ageCalculate);
